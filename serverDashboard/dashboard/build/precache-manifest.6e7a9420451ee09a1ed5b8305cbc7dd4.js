b1fafc77b1fb49064dd154343a85da6cdc5d1ae1
LeonelToledo111 <77387677+LeonelToledo111@users.noreply.github.com>
2021-03-24 23:54:12 +0100

    Add files via upload

---
+++ serverDashboard/dashboard/build/precache-manifest.6e7a9420451ee09a1ed5b8305cbc7dd4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78aae39a17989a3a6591b5c166440d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfcb56dd625f011e3bfe",
+    "url": "/static/css/2.ebead5ec.chunk.css"
+  },
+  {
+    "revision": "899ef1d48b8fcc99e09a",
+    "url": "/static/css/main.e5199e68.chunk.css"
+  },
+  {
+    "revision": "cfcb56dd625f011e3bfe",
+    "url": "/static/js/2.a639558b.chunk.js"
+  },
+  {
+    "revision": "ea54f8a8c118790d678b281da128c2b9",
+    "url": "/static/js/2.a639558b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "899ef1d48b8fcc99e09a",
+    "url": "/static/js/main.c925d262.chunk.js"
+  },
+  {
+    "revision": "fef0669c63847c5d06a0",
+    "url": "/static/js/runtime-main.e9665585.js"
+  },
+  {
+    "revision": "57bab6bdab3c51d289255458992e8101",
+    "url": "/static/media/countriesSorted.57bab6bd.csv"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6645e0ae07f268886fd4d733d1c52b0c",
+    "url": "/static/media/maize-yield-europe2.6645e0ae.geojson"
+  }
+]);
