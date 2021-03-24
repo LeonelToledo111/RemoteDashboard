8304480d8122a821a58351225515e877958e29a9
LeonelToledo111 <77387677+LeonelToledo111@users.noreply.github.com>
2021-03-24 23:04:59 +0100

    Add files via upload

---
+++ serverDashboard/dashboard/build/precache-manifest.a090df87249a3180f26e41358dd3425d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c131bce80ec638ae1a4979750adfeaaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "815676eeefb3a52c6bdc",
+    "url": "/static/css/2.ebead5ec.chunk.css"
+  },
+  {
+    "revision": "725638325670de055c56",
+    "url": "/static/css/main.92f9e487.chunk.css"
+  },
+  {
+    "revision": "815676eeefb3a52c6bdc",
+    "url": "/static/js/2.482a90e9.chunk.js"
+  },
+  {
+    "revision": "9b7ed8a7ae5b02d8b845a622c7d0ed10",
+    "url": "/static/js/2.482a90e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725638325670de055c56",
+    "url": "/static/js/main.b193aed5.chunk.js"
+  },
+  {
+    "revision": "fef0669c63847c5d06a0",
+    "url": "/static/js/runtime-main.e9665585.js"
+  },
+  {
+    "revision": "57bab6bdab3c51d289255458992e8101",
+    "url": "/static/media/countriesSorted.57bab6bd.csv"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
