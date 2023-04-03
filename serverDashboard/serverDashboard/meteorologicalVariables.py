67199d6a5f6aa59f46d0b060f36c5070f95ee693
LeonelToledo111 <77387677+LeonelToledo111@users.noreply.github.com>
2023-04-03 21:14:39 +0200

    Script fix

--- serverDashboard/serverDashboard/meteorologicalVariables.py
+++ serverDashboard/serverDashboard/meteorologicalVariables.py
@@ -483,7 +483,7 @@ def slurmWrapper(data):
    Longitude_min=data['_minLon']
    Longitude_max=data['_maxLon']
 
-   base = '/home/admin/PRODUCTION/Run/run -c ' + '"'+Country+'" ' +'-l' + '"'+str(Longitude_min)+'" '
+   base = '/home/admin/PRODUCTION/Run/run -c ' + '"'+Country+'" ' +'-l ' + '"'+str(Longitude_min)+'" '
    base += '-m ' + '"'+str(Longitude_max)+'" '
    base += '-n ' + '"'+str(Latitude_min)+'" '
    base += '-o ' + '"'+str(Latitude_max)+'" '
